f77f092c2ec0360013b544b6a4ef0364042bdfa5
soasme <soasme@gmail.com>
2017-08-22 22:25:57 +1200

    upgrade csv to sqlite.

--- techshack.py
+++ techshack.py
@@ -1,40 +1,90 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
 import argparse
 import csv
 import os
+import sqlite3
+from contextlib import contextmanager
 from datetime import datetime
 from io import StringIO
 from uuid import uuid4
 
 
-def save_stanza(uuid, ref, thoughts, tags):
-    """Save a piece of stanza.
+def prepare_database(conn):
+    """Create tables."""
+    cursor = conn.cursor()
+    cursor.execute("create table if not exists stanza (id text primary key, created text, ref text, thoughts text, tags text)")
+    conn.commit()
 
-    * ENV required: `STANZA_FILE_PATH`.
+
+@contextmanager
+def open_database():
     """
+    * ENV required: `STANZA_FILE_PATH`.
+
+    with open_database() as conn:
+        do_something(conn)
+    """
+    path = os.environ.get('STANZA_FILE_PATH')
+    conn = sqlite3.connect(path)
+    prepare_database(conn)
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+def get_stanza(conn, uuid):
+    cursor = conn.cursor()
+    cursor.execute("select * from stanza where id=?", (uuid, ))
+    return cursor.fetchone()
+
+
+def create_stanza(conn, ref):
+    cursor = conn.cursor()
+    uuid = str(uuid4())
+    created = datetime.utcnow().isoformat() + '+0000'
+    cursor.execute("insert into stanza(id,created,ref) values(?,?,?)", (uuid, created, ref, ))
+    conn.commit()
+    return uuid
+
+
+def set_stanza_thoughts(conn, uuid, thoughts):
+    cursor = conn.cursor()
+    cursor.execute("update stanza set thoughts=? where id=?", (thoughts, uuid, ))
+    conn.commit()
+
+
+def set_stanza_tags(conn, uuid, tags):
+    cursor = conn.cursor()
     tags = tags.replace(',', '|')
-    with open(os.environ.get('STANZA_FILE_PATH'), 'a') as f:
-        writer = csv.writer(f)
-        row = [
-            str(uuid),
-            datetime.utcnow().isoformat() + '+0000',
-            ref,
-            thoughts,
-            tags.replace(',', '|'),
-        ]
-        writer.writerow(row)
+    cursor.execute("update stanza set tags=? where id=?", (tags, uuid, ))
+    conn.commit()
 
 
-def get_stanza(uuid):
-    with open(os.environ.get('STANZA_FILE_PATH', 'w')) as f:
-        reader = csv.reader(f)
-        stanza = None
-        for row in reader:
-            if row[0] == uuid:
-                stanza = row
-        return stanza
+def format_stanza(stanza):
+    return """uuid: %s
+created: %s
+ref: %s
+thoughts: %s
+tags: %s""" % tuple(stanza)
+
+
+def start_stanza_session(uuid):
+    with open('/tmp/techshack.io.stanza.session', 'w') as f:
+        f.write(uuid)
+
+
+def destroy_stanza_session():
+    if os.path.exists('/tmp/techshack.io.stanza.session'):
+        os.remove('/tmp/techshack.io.stanza.session')
+
+
+def get_stanza_session():
+    if os.path.exists('/tmp/techshack.io.stanza.session'):
+        with open('/tmp/techshack.io.stanza.session', 'r') as f:
+            return f.read()
 
 
 def prog_slackbot(args, options):
@@ -48,38 +98,75 @@ def prog_slackbot(args, options):
     import re
     import json
 
-    @respond_to('echo (.*)', re.IGNORECASE)
-    def respond_to_github(message, something):
-        message.reply(something)
+    @respond_to('ping', re.IGNORECASE)
+    def respond_to_github(message):
+        message.reply('pong')
 
     @respond_to('show stanza (.*)', re.IGNORECASE)
     def show_stanza(message, uuid):
-        stanza = get_stanza(uuid)
-        if not stanza:
-            message.reply('stanza %s not found' % uuid)
-        else:
-            message.reply("""
-uuid: %s
-created: %s
-ref: %s
-thoughts: %s
-tags: %s
-            """ % tuple(stanza))
-
+        with open_database() as conn:
+            stanza = get_stanza(conn, uuid)
+            if not stanza:
+                message.reply('stanza %s not found' % uuid)
+            else:
+                message.reply(format_stanza(stanza))
 
     @respond_to('save stanza (.*)', re.IGNORECASE)
-    def save_stanza_to_dat(message, dat):
-        reader = csv.reader(StringIO(dat), delimiter=',')
-        row = next(reader)
-        if len(row) == 3:
-            uuid = uuid4()
-            ref, thoughts, tags = row
-            save_stanza(uuid, ref, thoughts, tags)
-            show_stanza(message, str(uuid))
-        elif len(row) == 4:
-            uuid, ref, thoughts, tags = row
-            save_stanza(uuid, ref, thoughts, tags)
-            show_stanza(message, str(uuid))
+    def save_stanza_to_dat(message, url):
+        with open_database() as conn:
+            uuid = create_stanza(conn, url)
+            start_stanza_session(uuid)
+            message.reply('Start editing %s' % uuid)
+
+    @respond_to('edit stanza (.*)', re.IGNORECASE)
+    def edit_stanza(message, uuid):
+        with open_database() as conn:
+            if get_stanza(conn, uuid):
+                start_stanza_session(uuid)
+                message.reply('Start editing %s' % uuid)
+            else:
+                message.reply('Stanza %s not found' % uuid)
+
+    @respond_to('done stanza')
+    def stop_editing_stanza(message):
+        with open_database() as conn:
+            uuid = get_stanza_session()
+            if uuid:
+                destroy_stanza_session()
+                message.reply('Quit editing %s' % uuid)
+                message.reply(format_stanza(get_stanza(conn, uuid)))
+            else:
+                message.reply('No session found.')
+
+    @respond_to('thoughts (.*)')
+    def set_stanza_thoughts_to_dat(message, thoughts):
+        with open_database() as conn:
+            uuid = get_stanza_session()
+            if uuid:
+                stanza = get_stanza(conn, uuid)
+                if stanza:
+                    set_stanza_thoughts(conn, uuid, thoughts)
+                    message.reply('Done')
+                else:
+                    destroy_stanza_session()
+                    message.reply('Stanza %s not found' % uuid)
+            else:
+                message.reply('No session found.')
+
+    @respond_to('tags (.*)')
+    def set_stanza_tags_to_dat(message, tags):
+        with open_database() as conn:
+            uuid = get_stanza_session()
+            if uuid:
+                stanza = get_stanza(conn, uuid)
+                if stanza:
+                    set_stanza_tags(conn, uuid, tags)
+                    message.reply('Done')
+                else:
+                    destroy_stanza_session()
+                    message.reply('Stanza %s not found' % uuid)
+            else:
+                message.reply('No session found.')
 
 
     bot = Bot()
