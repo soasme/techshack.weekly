b99ca7a5fc1a50f3245a3e5f893b093f900f8ef2
soasme <soasme@gmail.com>
2017-09-04 22:40:04 +1200

    add ga.

--- techshack.py
+++ techshack.py
@@ -109,6 +109,14 @@ SITE_TEMPLATE = """<!DOCTYPE html>
             $(".btn-explore").on('click', explore);
         });
         </script>
+        <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-36183732-2', 'auto');
+        ga('send', 'pageview');
+        </script>
 </html>"""
 
 DEFAULT_CONFIGS = ['/etc/techshack.io/env', './.env']
