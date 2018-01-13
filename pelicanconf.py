c14e729be5a3e1e753cd42257956e72bfdb61a57
soasme <soasme@gmail.com>
2018-01-13 19:52:04 +1300

    shift rss link to issues category.

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Ju Lin'
 SITENAME = 'Techshack Weekly'
-SITEURL = ''
+SITEURL = '/'
 
 PATH = 'content'
 
@@ -14,7 +14,7 @@ DEFAULT_LANG = 'zh'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = None
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
