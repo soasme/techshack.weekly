128742eaa8b669161d2be38e0eede85bb743f3d2
soasme <soasme@gmail.com>
2018-03-02 21:45:48 +1300

    remove simplenote part.

--- admin.py
+++ admin.py
@@ -12,7 +12,6 @@ import telegram
 import html
 from urllib.parse import unquote
 from datetime import datetime, timedelta
-from simplenote import Simplenote
 from apiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -32,163 +31,11 @@ verse_category: {{category}}
 {{content}}
 """
 
-sn = Simplenote(username=os.environ.get('SIMPLENOTE_USER'),
-                password=os.environ.get('SIMPLENOTE_PASS'))
 
 @click.group()
 def cli():
     pass
 
-
-def _parse_simplenote_note(content):
-    lines = content.splitlines()
-
-    def _is_verses_hint(ctx, line):
-        return line.strip() == '---'
-
-    def _is_uuid_hint(ctx, line):
-        return not ctx.get('uuid') and line.strip().startswith('* uuid:')
-
-    def _parse_uuid(ctx, line):
-        assert _is_uuid_hint(ctx, line), 'not a uuid hint line.'
-        return line.replace('* uuid:', '').strip()
-
-    def _is_date_hint(ctx, line):
-        return DATE_PATTERN.search(line)
-
-    def _parse_date(ctx, line):
-        assert _is_date_hint(ctx, line)
-        match = DATE_PATTERN.search(line)
-        assert match, 'no matching date: %s' % line
-        return match.group(1)
-
-    def _is_url_hint(ctx, line):
-        return not ctx.get('url') and line.startswith('* url:')
-
-    def _parse_url(ctx, line):
-        assert _is_url_hint(ctx, line)
-        match = URL_PATTERN.search(line)
-        assert match, 'no matching url: %s' % line
-        return match.group(2)
-
-    def _is_title_hint(ctx, line):
-        return not ctx.get('title') and line.startswith('* title:')
-
-    def _parse_title(ctx, line):
-        assert _is_title_hint(ctx, line)
-        return line.replace('* title:', '').strip()
-
-    def _is_category_hint(ctx, line):
-        return not ctx.get('category') and line.startswith('* category:')
-
-    def _parse_category(ctx, line):
-        assert _is_category_hint(ctx, line)
-        return line.replace('* category:', '').strip()
-
-    def _is_tags_hint(ctx, line):
-        return not ctx.get('tags') and line.startswith('* tags:')
-
-    def _parse_tags(ctx, line):
-        assert _is_tags_hint(ctx, line)
-        return line.replace('* tags:', '').strip()
-
-    def _is_hint(ctx, line):
-        return 'content' not in ctx
-
-    def _is_hint_end(ctx, line):
-        return 'uuid' in ctx and 'url' in ctx and not line
-
-    def _is_valid_ctx(ctx):
-        return 'uuid' in ctx and 'date' in ctx and 'url' in ctx and 'content' in ctx
-
-
-    ctx = {}
-
-    for line in lines:
-        line = unquote(line)
-        if _is_verses_hint(ctx, line): # start parse verse
-            if _is_valid_ctx(ctx):
-                yield {'date': ctx['date'], 'uuid': ctx['uuid'],
-                       'url': ctx['url'], 'title': ctx.get('title'),
-                       'category': ctx.get('category'),
-                       'content': '\n'.join(ctx['content'])}
-            if 'date' not in ctx:
-                break # Invalid post
-            ctx = {'date': ctx['date']}
-        elif _is_hint(ctx, line): # parse verse metadata
-            if _is_date_hint(ctx, line):
-                ctx['date'] = _parse_date(ctx, line)
-            elif _is_uuid_hint(ctx, line):
-                ctx['uuid'] = _parse_uuid(ctx, line)
-            elif _is_url_hint(ctx, line):
-                ctx['url'] = _parse_url(ctx, line)
-            elif _is_title_hint(ctx, line):
-                ctx['title'] = _parse_title(ctx, line)
-            elif _is_category_hint(ctx, line):
-                ctx['category'] = _parse_category(ctx, line)
-            elif _is_tags_hint(ctx, line):
-                ctx['tags'] = _parse_tags(ctx, line)
-            elif _is_hint_end(ctx, line):
-                ctx.setdefault('content', [])
-        else: # parse verse content
-            ctx['content'].append(html.unescape(line))
-
-    if _is_valid_ctx(ctx):
-        yield {'date': ctx['date'], 'uuid': ctx['uuid'],
-                'url': ctx['url'], 'title': ctx.get('title'),
-                'tags': ctx.get('tags'), 'category': ctx.get('category'),
-                'content': '\n'.join(ctx['content'])}
-
-
-def is_valid_verse(verse):
-    return verse.get('title') and verse.get('content') and \
-            verse.get('uuid') and verse.get('date')
-
-def _generate_verse(verse):
-
-    if not is_valid_verse(verse):
-        print('WARNING: invalid verse: %s' % verse)
-        return
-
-    filename = 'content/verses/%s.md' % verse['uuid']
-    with open(filename, 'w') as f:
-        markdown = jinja2.Template(VERSE_TEMPLATE).render(verse)
-        f.write(markdown)
-        print('>>>>>>>>> Generating Verse %s <<<<<<<<<<' % filename)
-        print(markdown)
-
-
-def _generate_jsonrow(verses):
-    print(json.dumps([{
-        'key': verse['uuid'],
-        'title': verse.get('title') or 'No-title',
-        'text': verse['content'],
-        'url': verse['url'],
-        'type': 'verse',
-        'date': verse['date'],
-        'category': verse.get('category', 'non-categorized'),
-        'tags': verse.get('tags')
-    } for verse in verses], ensure_ascii=False))
-
-@cli.command()
-@click.option('--since', help='Format: YYYY-MM-DD')
-@click.option('--format', default='markdown')
-def import_simplenote(since, format):
-    notes, status = sn.get_note_list(since=since, tags=[TECHSHACK_SIMPLENOTE_TAG])
-    assert status != -1, 'Get techshack simplenote notes failed.'
-    if format == 'markdown':
-        for _note in notes:
-            note, status = sn.get_note(_note['key'])
-            assert status != -1, 'Get techshack simplenote note {} failed.'.format(_note['key'])
-            for verse in _parse_simplenote_note(note['content']):
-                _generate_verse(verse)
-    elif format == 'jsonrow':
-        verses = []
-        for _note in notes:
-            note, status = sn.get_note(_note['key'])
-            assert status != -1, 'Get techshack simplenote note {} failed.'.format(_note['key'])
-            verses.extend(list(_parse_simplenote_note(note['content'])))
-        _generate_jsonrow(verses)
 
 GA_SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 def get_ga_stats(date):
